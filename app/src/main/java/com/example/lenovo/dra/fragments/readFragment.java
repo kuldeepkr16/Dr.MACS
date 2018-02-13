2d8a773719ea9b0c4e3a744a0a8d1e1d9ab45d94
kuldeep <kuldeep@github.com>
2018-02-13 10:39:31 +0530

    fourth commit

--- app/src/main/java/com/example/lenovo/dra/fragments/readFragment.java
+++ app/src/main/java/com/example/lenovo/dra/fragments/readFragment.java
@@ -1,12 +1,17 @@
 package com.example.lenovo.dra.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.lenovo.dra.Activities.MedicineAvailibility;
 import com.example.lenovo.dra.R;
 
 
@@ -14,7 +19,7 @@ import com.example.lenovo.dra.R;
  * A simple {@link Fragment} subclass.
  */
 public class readFragment extends Fragment {
-
+    Button txtNewPost;
 
     public readFragment() {
         // Required empty public constructor
@@ -28,4 +33,14 @@ public class readFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_read, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        txtNewPost = (Button) view.findViewById(R.id.txtNewPost);
+        txtNewPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MedicineAvailibility.class));
+            }
+        });
+    }
 }
